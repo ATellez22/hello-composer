d3ea1cc12098c3893c8d2e786c852477df3e3dae
ATellez22 <61438075+ATellez22@users.noreply.github.com>
2023-01-22 15:16:31 -0300

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lic/hello-composer',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2b2b52fde4fdcac8e5d43fe7c81e267da5b2f606',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lic/hello-composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2b2b52fde4fdcac8e5d43fe7c81e267da5b2f606',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
